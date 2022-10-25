a1948c021f313ca01975a4fc45d578e8eb060f43
Daan Roelofs <daanroelofs599@gmail.com>
2022-10-25 20:32:55 +0200

    test 101

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import HomePage from "./components/Homepage/HomePage";
+// import HomePage from "./components/Homepage/HomePage";
 import PersoonlijkePodcasts from "./components/Persoonlijkepodcasts/PersoonlijkePodcasts";
 import ZakelijkePodcasts from "./components/Zakelijkepodcasts/ZakelijkePodcasts";
 import Makers from "./components/Makers/Makers";
@@ -6,7 +6,7 @@ import Makers from "./components/Makers/Makers";
 function App() {
   return (
     <>
-      <HomePage />
+      {/* <HomePage /> */}
       <PersoonlijkePodcasts />
       <ZakelijkePodcasts />
       <Makers />
